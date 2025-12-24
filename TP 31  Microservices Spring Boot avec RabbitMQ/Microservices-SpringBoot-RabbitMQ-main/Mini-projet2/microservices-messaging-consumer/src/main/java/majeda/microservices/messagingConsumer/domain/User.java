22963377493c3fb1d10ac45419cf72033029e9bc
kaitlbiz9752-sudo <k.aitlbiz9752@uca.ac.ma>
2025-12-24 02:51:22 +0100

    Add files via upload

---
+++ TP 31  Microservices Spring Boot avec RabbitMQ/Microservices-SpringBoot-RabbitMQ-main/Mini-projet2/microservices-messaging-consumer/src/main/java/majeda/microservices/messagingConsumer/domain/User.java
@@ -0,0 +1,25 @@
+package majeda.microservices.messagingConsumer.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+    private String userName;
+}
